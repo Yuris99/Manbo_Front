a54365eaddc2cee7d031eeb03c771fc56889d787
Yuris99 <mose1207@gmail.com>
2024-05-13 22:19:44 +0900

    Create navigation bar & types datatype

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+import Colors from '@constants/Colors';
+
+
+
+export default function TabLayout() {
+  const colorScheme = 'light';
+  const iconSize = Platform.OS == 'ios' ? 32 : 28;
+
+  return (    
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: Platform.OS == 'ios' ? {
+          height: '9.5%',
+        } : {
+          height: '6.5%',
+        },
+        tabBarIconStyle: {
+        },
+      }}
+    >
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: 'home',
+        tabBarIcon: ({ color }) => 
+        <MaterialCommunityIcons 
+          name="home" 
+          color={color} 
+          size={iconSize} 
+        />,
+      }}
+    />
+    <Tabs.Screen
+      name="community"
+      options={{
+        title: 'community',
+        tabBarIcon: ({ color }) => 
+          <MaterialCommunityIcons 
+            name="message-text" 
+            color={color} 
+            size={iconSize} 
+          />,
+      }}
+    />
+    <Tabs.Screen
+      name="trail"
+      options={{
+        title: 'trail',
+        tabBarIcon: ({ color }) => 
+          <MaterialCommunityIcons 
+            name="walk" 
+            color={color} 
+            size={iconSize} 
+          />,
+      }}
+    />
+    <Tabs.Screen
+      name="mypage"
+      options={{
+        title: 'home',
+        tabBarIcon: ({ color }) => 
+          <MaterialCommunityIcons 
+            name="account" 
+            color={color} 
+            size={iconSize} 
+          />,
+      }}
+    />
+    </Tabs>
+  );
+}
+
+/*
+
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'test',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="three"
+        options={{
+          title: 'three',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs> */
---
+++ src/types.ts
@@ -0,0 +1,142 @@
+/** 사용자 데이터 타입
+ * 
+ * @property {number} id - 유저id(db식별번호, 가려짐)
+ * @property {string} username - 유저 닉네임
+ * @property {string} email - 유저 이메일(로그인 아이디)
+ * @property {string} gender - 성별 (M | F)
+ * @property {number} age - 나이
+ */
+export type User = {
+    id: number;
+    username: string;
+    email: string;
+    gender: string;
+    age: number;
+};
+
+/** 주소 설정 데이터 타입 (enum으로 바꿀 예정)
+ * 
+ * @property {string} city - 시
+ * @property {string} town - 군
+ * @property {string} village - 구
+ */
+export type Location = {
+    city: string;
+    town: string;
+    village: string;
+};
+
+/** 산책로 태그 Enum
+ *  추가예정
+ */
+export type TrailTag = 'tag1' | 'tag2';
+
+/** 산책로 통합 데이터타입
+ * 
+ * @property {number} id - 산책로 id
+ * @property {number} user_id - 제작자 id
+ * @property {string} startloc - 시작위치(좌표값)
+ * @property {Location} location - 산책로 위치(시군구)
+ * @property {string} name - 산책로 이름
+ * @property {number} walks - 산책로 이용 횟수
+ * @property {number} rank - 산책로 평점
+ * @property {number} created - 등록일
+ * @property {TrailTag[]} tags - 산책로 태그 리스트
+ */
+export type Trail = {
+    id: number;
+    user_id: number;
+    startloc: string;
+    location: Location;
+    name: string;
+    walks: number;
+    rank: number;
+    created: Date;
+    tags: TrailTag[];
+};
+
+/** 게시글 종류 ENUM
+ * 
+ */
+export type PostType = 'Free' | 'Recommend' | "Announcement";
+
+/** 게시글 통합 데이터타입
+ * 
+ * @property {PostType} type - 게시글 종류 ('Free' | 'Recommend' | "Announcement")
+ * @property {number} id - 게시글 id
+ * @property {number} user_id - 작성자 id
+ * @property {number} trail_id - 산책로 id (Recommend 게시판에서만 사용)
+ * @property {string} title - 게시글 제목
+ * @property {string} content - 게시글 내용
+ * @property {Date} created - 게시 날짜 및 시간
+ * @property {number} view - 조회수
+ * @property {number} like - 좋아요수
+ */
+export type Post = {
+    type: PostType;
+    id: number;
+    user_id: number;
+    trail_id: number | null;
+    title: string;
+    content: string;
+    created: Date;
+    view: number;
+    like: number;
+};
+
+/** 댓글 통합 데이터타입
+ * 
+ * @property {PostType} type - 게시글 종류 ('Free' | 'Recommend' | "Announcement")
+ * @property {number} id - 댓글 id
+ * @property {number} post_id - 게시글 id
+ * @property {number} user_id - 작성자 id
+ * @property {string} content - 댓글 내용
+ * @property {Date} created - 댓글 게시 날짜 및 시간
+ */
+export type Comment = {
+    type: PostType;
+    id: number;
+    post_id: number;
+    user_id: number;
+    content: string;
+    created: Date;
+};
+
+/** 이미지 통합 데이터타입
+ * 
+ * @property {PostType} type - 게시글 종류 ('Free' | 'Recommend' | "Announcement")
+ * @property {number} post_id - 게시글 id
+ * @property {string} file_path - 이미지 주소
+ */
+export type PostImage = {
+    type: PostType;
+    post_id: number;
+    file_path: string;
+};
+
+/** 그룹 상태 ENUM
+ * 
+ */
+export type RoomState = 'Open' | 'Full' | 'Walking' | 'Close';
+
+/** 그룹 데이터타입
+ * 
+ * @property {number} id - 방번호
+ * @property {number} host_id - 주최자 id
+ * @property {number} trail_id - 산책로 id
+ * @property {string} name - 방 이름
+ * @property {string} content - 방 설명
+ * @property {number} maximum - 최대인원
+ * @property {Date} meettime - 모임날짜
+ * @property {RoomState} state - 방상태
+ */
+export type Room = {
+    id: number;
+    host_id: number;
+    trail_id: number;
+    name: string;
+    content: string;
+    maximum: number;
+    meettime: Date;
+    state: RoomState;
+}
