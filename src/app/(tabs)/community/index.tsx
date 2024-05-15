50c7119ce6a9aa6b39b2ac3545786c720dd01b52
NEO <hojoong0210@gmail.com>
2024-05-15 16:33:20 +0900

    what chagnge?

--- src/app/(tabs)/community/index.tsx
+++ src/app/(tabs)/community/index.tsx
@@ -8,7 +8,7 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Community</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/community/index.tsx" />
+      <EditScreenInfo path="웰컴투 서울" />
     </View>
   );
 }
