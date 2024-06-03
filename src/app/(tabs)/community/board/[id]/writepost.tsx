871ba217977b96a9fe3bb3d2967e1b4382a4ecb9
NEO <hojoong0210@gmail.com>
2024-06-03 13:10:43 +0900

    Update Write Post

--- src/app/(tabs)/community/board/[id]/writepost.tsx
+++ src/app/(tabs)/community/board/[id]/writepost.tsx
@@ -1,17 +1,32 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@components/EditScreenInfo';
-import { Text, View } from '@components/Themed';
-
+import { View, FlatList, StyleSheet, TextInput, Pressable, Text, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 export default function NoticeBoard() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>writepage</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/mypage/index.tsx" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <TextInput
+        placeholder='제목을 입력하세요'
+        style={styles.inputtitle}
+        multiline
+        numberOfLines={2}
+        blurOnSubmit={true}
+        />
+        <Text>
+          제목은 3글자 이상 작성하여주세요.
+        </Text>
+        <View style={styles.inputcontentview}>
+          <TextInput
+            placeholder='커뮤니티 이용규칙을 준수하여 작성하여주세요'
+            style={styles.inputcontent}
+            multiline />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
+const titlesize = Platform.OS == 'ios' ? 25 : 20;
+const contentsize = Platform.OS == 'ios' ? 18 : 15;
 
 const styles = StyleSheet.create({
   container: {
@@ -19,13 +34,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  inputtitle: {
+    backgroundColor: 'white',
+    marginTop: 10,
+    width: '90%',
+    borderWidth: 1,
+    borderColor: '#aaaaaa',
+    borderRadius: 20,
+    height: '15%',
+    fontSize: titlesize,
+    padding: 20,
+    textAlignVertical: 'top',
+    minHeight: '10%',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  inputcontentview: {
+    backgroundColor: 'white',
+    width: '90%',
+    borderWidth: 1,
+    borderColor: '#aaaaaa',
+    borderRadius: 20,
+    marginTop: 10,
+    padding: 20,
+    flex: 1,
+  },
+  inputcontent: {
+    fontSize: contentsize,
+    textAlignVertical: 'top',
   },
 });
