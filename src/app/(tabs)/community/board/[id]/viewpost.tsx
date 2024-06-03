75a54996a3b495898f21d6678f2a5b98bd6fb095
NEO <hojoong0210@gmail.com>
2024-06-03 14:13:20 +0900

    Update Write and View Post

--- src/app/(tabs)/community/board/[id]/viewpost.tsx
+++ src/app/(tabs)/community/board/[id]/viewpost.tsx
@@ -1,14 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { View, Text, FlatList, Image, Pressable, StyleSheet } from 'react-native';
+import FreePost from '@assets/testdata/freedata';
+import { Post } from '@/src/types'
+import { Stack, useLocalSearchParams } from 'expo-router';
 
-import EditScreenInfo from '@components/EditScreenInfo';
-import { Text, View } from '@components/Themed';
 
 export default function NoticeBoard() {
+  const {user_id} = useLocalSearchParams();
+  const post = FreePost[Number(user_id)];
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>viewpage</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/mypage/index.tsx" />
+      <Text style={styles.title}>{post.title}</Text>
+     <View style={styles.userContainer}> 
+        <Text style={styles.username}>{post.user_id}</Text>
+        <Text style={styles.userinfo}>{post.created.getMonth()+1}/{post.created.getDate()} {post.created.getHours() >=12 ? '오후' : '오전'} {post.created.getHours() > 12 ? post.created.getHours()-12 :(post.created.getHours() == 0 ? 12 : post.created.getHours())}:{post.created.getMinutes() < 10 ?'0'+post.created.getMinutes() : post.created.getMinutes()  }</Text>
+        <Text style={styles.userinfo}>{post.view}  {post.like}</Text>
+      </View>
+      <View style={styles.userContent}>
+        <Text style={styles.content}>{post.content}</Text>
+      </View>
     </View>
   );
 }
@@ -19,13 +28,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  userContainer: {
+   flex:1,
+   alignItems: 'center'
+  },
+  userContent: {
+    flex:1,
+    alignItems:'center',
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  username: {
+    fontSize: 10,
+    fontWeight: 'bold',
   },
+  userinfo: {
+    fontSize: 10,
+  },
+  content: {
+    fontSize:15,
+    fontWeight: 'bold'
+  }
 });
